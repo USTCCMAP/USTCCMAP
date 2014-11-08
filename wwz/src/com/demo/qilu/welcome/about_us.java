cbdcfd526df20fc6b8c067bfe01fdae539ccd5da
SARSvsSARS <962165118@qq.com>
2014-11-08 21:48:50 +0800

    update weather pare

---
+++ wwz/src/com/demo/qilu/welcome/about_us.java
@@ -0,0 +1,25 @@
+/**
+ * @author 神奇4+1
+ * @version 1.0.0 创建时间：2012-08-08
+ * 该类为本项目的“关于我们”界面。
+ * 该类介绍了团队的基本信息。
+ * 包括：成员，成员分工等。
+ */
+package com.demo.qilu.welcome;
+
+import com.demo.qilu.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class about_us  extends Activity {
+
+		/**
+		 * 关于我们
+		 */
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.about_us);
+       }
+        
+}
